d0ce3ef43c53eae64037db20b57c883d21effa90
Puneet Gupta <gupta.puneet0696@gmail.com>
2017-08-26 03:07:19 +0530

    Added ngrok
    Signed-off-by: Puneet Gupta <gupta.puneet0696@gmail.com>

--- backend/Ehaat/Ehaat/settings.py
+++ backend/Ehaat/Ehaat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*4+7q-8h(g()t&d8mhxrlw35+ns1hnddwb91#^t#&)m39hy72+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
