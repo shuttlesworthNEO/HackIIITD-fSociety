66a6d68481288f10cc6b18d79f87819a3bc85e5e
Puneet Gupta <gupta.puneet0696@gmail.com>
2017-08-26 02:32:13 +0530

    Added info APIs
    Signed-off-by: Puneet Gupta <gupta.puneet0696@gmail.com>

--- backend/Ehaat/details/urls.py
+++ backend/Ehaat/details/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import haatList
+from .views import haatList, stallList, productList
 
 urlpatterns = [
 	url(r'api/haat', haatList),
+	url(r'api/stall', stallList),
+	url(r'api/product', productList),
 ]
--- backend/Ehaat/details/views.py
+++ backend/Ehaat/details/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, JsonResponse
 from django.core.serializers import serialize
-from .models import Haat
+from .models import Haat, Stall, Product
 # Create your views here.
 
 def haatList(request):
@@ -15,4 +15,32 @@ def haatList(request):
 		temp["info"] = haat.info
 		temp["address"] = haat.address
 		jsonObj.append(temp)
-	return JsonResponse({"haats" : jsonObj})
+	return JsonResponse({"haats" : jsonObj}, safe=False)
+
+def stallList(request):
+	stalls = Stall.objects.all()
+	jsonObj = []
+	for stall in stalls:
+		temp = dict()
+		temp["haatID"] = stall.haat
+		temp["stallID"] = stall.id
+		temp["name"] = stall.name
+		temp["stallNumber"] = stall.stallNumber
+		temp["story"] = stall.story
+		temp["tags"] = stall.tags.split(',')
+		jsonObj.append(temp)
+	return JsonResponse({"stalls" : jsonObj}, safe=False)
+
+def productList(request):
+	products = Product.objects.all()
+	jsonObj = []
+	for product in products:
+		temp = dict()
+		temp["stallID"] = product.stall
+		temp["productID"] = product.id
+		temp["name"] = product.name
+		temp["description"] = product.description
+		temp["price"] = product.price
+		temp["promotions"] = product.promotions
+		jsonObj.append(temp)
+	return JsonResponse({'products' : temp}, safe=False)
