4d07bfdb92210b4e63b1f8df0637180d43aad84c
guptachetan1997 <gupta.chetan1997@gmail.com>
2017-08-26 01:57:26 +0530

    secondCommit

--- backend/Ehaat/details/views.py
+++ backend/Ehaat/details/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
+from django.core.serializers import serialize
+from .models import Haat
 # Create your views here.
 
 def haatList(request):
-	return HttpResponseRedirect("http://google.com")
-
+	haats = Haat.objects.all()
+	jsonObj = []
+	for haat in haats:
+		temp = dict()
+		temp["haatID"] = haat.id
+		temp["name"] = haat.name
+		temp["theme"] = haat.theme
+		temp["info"] = haat.info
+		temp["address"] = haat.address
+		jsonObj.append(temp)
+	return JsonResponse({"haats" : jsonObj})
